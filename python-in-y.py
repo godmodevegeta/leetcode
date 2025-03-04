e28186f26cb744cc17efbcea42a003c38b9e6170
godmodevegeta <kshubham1904@gmail.com>
2025-03-04 18:21:06 +0530

    Enhance list operations in python-in-y.py by adding new list manipulations and print statements

--- python-in-y.py
+++ python-in-y.py
@@ -3,5 +3,22 @@ li = []
 for i in range(9):
     li.append(i + 3)
 
-# print(li)
-print(li[:])
+li2 = li[:]
+# print (li == li2)
+# print (li is li2)
+
+li2 = li
+# print(li2)
+
+# print (li == li2)
+# print (li is li2)
+
+li_f = []
+for i in li:
+    li_f.append(i + 0.1)
+
+print (li_f)
+
+
+li.extend(li_f)
+print (li)
