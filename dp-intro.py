5264e3693f9435d0f200b60e01eb59d7966fec51
godmodevegeta <kshubham1904@gmail.com>
2026-01-04 12:36:12 +0530

    Add dynamic programming functions: gridTraveller, howSum, bestSum, and canConstruct

--- dp-intro.py
+++ dp-intro.py
@@ -6,4 +6,72 @@ def fib(n, memo = {}):
     memo[n] = a + b
     return memo[n]
 
-print(fib(79))
+def gridTraveller(m, n, memo = {}):
+    key = str(m) + "," + str(n)
+    # check memo
+    if key in memo: return memo[key]
+    if m == 0 or n == 0:
+        return 0
+    if m == 1 and n == 1:
+        return 1
+    # go down
+    ways_down = gridTraveller(m - 1, n)
+    # go right
+    ways_right = gridTraveller(m, n - 1)
+    memo[key] = ways_down + ways_right
+    return memo[key]
+    
+def howSum(target, numbers, memo = {}):
+    key = target
+    if key in memo: return memo[key]
+    if target == 0: return []
+    if target < 0: return None
+    for n in numbers:
+        remainder = target - n
+        li = howSum(remainder, numbers)
+        if li is not None:
+            memo[key] = li + [n]
+            return memo[key]
+    memo[key] = None
+    return None
+
+def bestSum(target, numbers) :
+    if target == 0: return []
+    if target < 0: return None
+
+    resultCombination = None
+    for num in numbers:
+        remainder = target - num
+        combination = bestSum(remainder, numbers)
+        remainderCombination = 1
+        if (resultCombination == None) or len(remainderCombination) < len(resultCombination):
+            resultCombination = remainderCombination
+    return resultCombination
+
+def canConstruct(target, wordBank, memo = {}):
+    if target in memo: return memo[target]
+    if target == "": return True
+    
+    memo[target] = False
+    for word in wordBank:
+        wordLength = len(word)
+        if word == target[:wordLength]:
+            remainderTarget = target[wordLength:]
+            if canConstruct(remainderTarget, wordBank):
+                memo[target] = True
+                return memo[target]
+    return memo[target]
+    
+
+
+if __name__ == "__main__":
+    print(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+    print(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+    print(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+        "e",
+        "ee", 
+        "eee", 
+        "eeee", 
+        "eeeeeee"]))
+    
+    pass
